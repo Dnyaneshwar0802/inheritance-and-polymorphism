ae58787b7559d5baafc9cc54a0f3f92c419c6d98
GroupProject-D <dnyaneshwarbagal0802@gmail.com>
2025-01-15 11:25:47 +0530

    Single Inheritance

--- Simpleinheritance/singleinheritance/Main.java
+++ Simpleinheritance/singleinheritance/Main.java
@@ -5,6 +5,7 @@ package singleinheritance;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
-
+        Car car = new Car(150, "Toyota", "Corolla");
+        car.displayInfo();
     }
 }
