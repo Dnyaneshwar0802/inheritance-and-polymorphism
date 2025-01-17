681b317f16020d4a2972c859373c366263c82375
GroupProject-D <dnyaneshwarbagal0802@gmail.com>
2025-01-17 11:04:29 +0530

    Method Overloading example

--- MethodOverloading/MethodOverloadingTest.java
+++ MethodOverloading/MethodOverloadingTest.java
@@ -2,6 +2,12 @@
 public class MethodOverloadingTest {
     public static void main(String[] args) {
         System.out.println("This is Test class");
+        MathOperations math = new MathOperations();
 
+        int productOfTwo = math.multiply(4, 5);
+        int productOfThree = math.multiply(2, 3, 4);
+
+        System.out.println("Product of two numbers: " + productOfTwo);
+        System.out.println("Product of three numbers: " + productOfThree);
     }
 }
